9a0e99234ff7bd43d2bed66104b9408477288ed7
Min Xiang <cff29546@gmail.com>
2022-04-08 15:15:56 +0800

    fix evergreen tree textures

--- pzmap2dzi/plants.py
+++ pzmap2dzi/plants.py
@@ -13,6 +13,26 @@ _TREE_DEF = [
     ('American Linden', 'e_americanlinden', False),
 ]
 
+def get_tree(prefix, season, snow, size, evergreen):
+    is_jumbo = size >= 4
+    idx = size % 4
+    prefix += 'JUMBO_1_' if is_jumbo else '_1_'
+    step = 2 if is_jumbo else 4 
+    if snow:
+        textures = [prefix + str(idx + step)]
+    else:
+        textures = [prefix + str(idx)]
+        if not evergreen:
+            if season == 'spring':
+                textures.append(prefix + str(idx + step * 2))
+            if season == 'summer':
+                textures.append(prefix + str(idx + step * 3))
+            if season == 'summer2':
+                textures.append(prefix + str(idx + step * 4))
+            if season == 'autumn':
+                textures.append(prefix + str(idx + step * 5))
+    return textures
+
 class PlantsInfo(object):
     def __init__(self, season='summer2', snow=False, flower=False, large_bush=False,
                  tree_size=2, jumbo_size=3, jumbo_type=3):
@@ -36,17 +56,18 @@ class PlantsInfo(object):
             if large_bush:
                 offset1 = 8
                 offset2 = 32
-            textures.append(_bush.format(trunk + offset1))
-            if season == 'spring':
-                textures.append(_bush.format(trunk + offset1 + 32))
-            if season in ['summer', 'summer2']:
-                textures.append(_bush.format(i + offset2 + 64))
-            if season == 'autumn':
-                textures.append(_bush.format(trunk + offset1 + 48))
-            if flower:
-                textures.append(_bush.format(i + offset2 + 80))
             if snow:
                 textures = [_bush.format(trunk + offset1 + 16)]
+            else:
+                textures = [_bush.format(trunk + offset1)]
+                if season == 'spring':
+                    textures.append(_bush.format(trunk + offset1 + 32))
+                if season in ['summer', 'summer2']:
+                    textures.append(_bush.format(i + offset2 + 64))
+                if season == 'autumn':
+                    textures.append(_bush.format(trunk + offset1 + 48))
+                if flower:
+                    textures.append(_bush.format(i + offset2 + 80))
             bush.append(textures)
 
         for i in range(16):
@@ -93,31 +114,15 @@ class PlantsInfo(object):
         tree = []
         tree_size = max(0, min(3, int(tree_size)))
         for _, prefix, evergreen in _TREE_DEF:
-            idx = tree_size % 4
-            stages = 4
-            textures = ['{}_1_{}'.format(prefix, idx)]
-            if season == 'spring':
-                textures.append('{}_1_{}'.format(prefix, idx + stages * 2))
-            if season == 'summer':
-                textures.append('{}_1_{}'.format(prefix, idx + stages * 3))
-            if season == 'summer2':
-                textures.append('{}_1_{}'.format(prefix, idx + stages * 4))
-            if season == 'autumn':
-                textures.append('{}_1_{}'.format(prefix, idx + stages * 5))
-            if snow:
-                textures = ['{}_1_{}'.format(prefix, idx + stages)]
- 
-            tree.append(textures)
-        
+            tree.append(get_tree(prefix, season, snow, tree_size, evergreen))
+
         # base on game version 41.68, only following vegetation_trees tiles are used:
         # vegetation_trees_01_0, _2, _3
         # vegetation_trees_01_8 ~ _15
         # 0, 2, 3 : tree 0, 1, 2
-        for i in range(0, 4):
-            if i > 1:
-                self.mapping['vegetation_trees_01_{}'.format(i)] = tree[i - 1]
-            if i < 1:
-                self.mapping['vegetation_trees_01_{}'.format(i)] = tree[i]
+        self.mapping['vegetation_trees_01_0'] = tree[0]
+        self.mapping['vegetation_trees_01_2'] = tree[1]
+        self.mapping['vegetation_trees_01_3'] = tree[2]
 
         # 8 ~ 15 : tree 3 ~ 10
         for i in range(8, 16):
@@ -125,24 +130,8 @@ class PlantsInfo(object):
 
         # jumbo tree
         jumbo_size = max(tree_size, min(5, int(jumbo_size)))
-        idx = jumbo_size % 4
-        prefix = _TREE_DEF[jumbo_type][1]
-        stages = 4
-        if jumbo_size >= 4:
-            prefix += 'JUMBO'
-            stages = 2
-        textures = ['{}_1_{}'.format(prefix, idx)]
-        if season == 'spring':
-            textures.append('{}_1_{}'.format(prefix, idx + stages * 2))
-        if season == 'summer':
-            textures.append('{}_1_{}'.format(prefix, idx + stages * 3))
-        if season == 'summer2':
-            textures.append('{}_1_{}'.format(prefix, idx + stages * 4))
-        if season == 'autumn':
-            textures.append('{}_1_{}'.format(prefix, idx + stages * 5))
-        if snow:
-            textures = ['{}_1_{}'.format(prefix, idx + stages)]
-        self.mapping['jumbo_tree_01_0'] = textures
+        _, prefix, evergreen = _TREE_DEF[jumbo_type]
+        self.mapping['jumbo_tree_01_0'] = get_tree(prefix, season, snow, jumbo_size, evergreen)
 
             
 

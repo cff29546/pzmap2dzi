3578a5475a35f8674edff2c2df5555c6d5fa5680
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2025-01-25 10:37:58 +0100

    support map name prefixes

--- html/pzmap.js
+++ html/pzmap.js
@@ -249,7 +249,7 @@ function initOSD() {
         drawer: 'canvas',
         opacity: 1,
         element: document.getElementById('map_div'),
-        tileSources:  'base' + g.base_map.suffix + '/layer0.dzi',
+        tileSources: g.prefix + 'base' + g.base_map.suffix + '/layer0.dzi',
         homeFillsViewer: true,
         showZoomControl: true,
         constrainDuringPan: true,
--- html/pzmap/globals.js
+++ html/pzmap/globals.js
@@ -6,8 +6,17 @@ export var g = {
     trimmerui: 0,
     markerui: 0,
     aboutui: 0,
-    currentLayer: 0
+    currentLayer: 0,
+		get: {}
 };
+function populateGet() {
+  var obj = {}, params = location.search.slice(1).split('&');
+  for(var i=0,len=params.length;i<len;i++) {
+    var keyVal = params[i].split('=');
+    obj[decodeURIComponent(keyVal[0])] = decodeURIComponent(keyVal[1]);
+  }
+  return obj;
+}
 export function initGlobals() {
     g.viewer = 0;
     g.base_map = 0;
@@ -16,4 +25,8 @@ export function initGlobals() {
     g.minLayer = 0;
     g.maxLayer = 0;
     g.grid = 0;
+		g.get = populateGet();
+    if (undefined == g.get['map_name']){
+			g.get['map_name'] = 'SurvivalB4202';
+		}
 };
